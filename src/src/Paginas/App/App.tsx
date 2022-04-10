19dfd21113959f12e310e9b75ac9fecdd79538e2
Daniel Santos <iam@w-b.dev>
2022-04-10 10:23:43 -0300

    adiciona cabecalho e rodape

--- src/src/Paginas/App/App.tsx
+++ src/src/Paginas/App/App.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import "./App.css";
 import { Cabecalho } from "../../Componentes/Cabecalho/Cabecalho";
+import { ContainerCentral } from "../../Componentes/ContainerCentral/ContainerCentral";
+import { Rodape } from "../../Componentes/Rodape/Rodape";
 
 const App = () => (
   <div className="App">
     <Cabecalho />
+    <ContainerCentral />
+    <Rodape />
   </div>
 );
 
