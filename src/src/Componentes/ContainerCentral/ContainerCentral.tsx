8478f8d438585d4b9e037de4ccb4a21041e3a49c
Daniel Santos <iam@w-b.dev>
2022-04-10 10:21:10 -0300

    add cabecalho e rodape

---
+++ src/src/Componentes/ContainerCentral/ContainerCentral.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./ContainerCentral.css";
+
+const ContainerCentral = () => (
+  <div className="ContainerCentral">ContainerCentral</div>
+);
+
+export { ContainerCentral };
---
+++ src/src/Componentes/Rodape/Rodape.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./Rodape.css";
+
+const Rodape = () => <div className="Rodape">Rodape</div>;
+
+export { Rodape };
