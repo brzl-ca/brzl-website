2df09d14dd5af52f6a01d165894d52a377cefa56
Daniel Santos <iam@w-b.dev>
2022-04-19 13:43:17 -0300

    add cassandra-related comments

--- src/src/Paginas/Blog/Blog.tsx
+++ src/src/Paginas/Blog/Blog.tsx
@@ -98,6 +98,48 @@ const Blog = () => {
           Mais realisticamente, essa seção visa agrupar alguns posts e links
           interessantes.
         </p>
+        <section>
+          A estrutura dos 3 itens que compoem o *PRIMARY KEY* tabela de Posts:
+          <ol>
+            <li>DATA DA CRIACAO (SEM TIMESTAMP UNICO -- uso OO:00:00) - este é o *partition key*</li>
+            <li>TIMESTAMP - está é uma *clustering column*, que ordena os resultados</li>
+            <li>ID - está é uma *clustering column*, que ordena os resultados</li>
+          </ol>
+          <p>
+            O partition key define qual o node no cluster vai armazenar o valor.
+            <br/>
+            A primary key (óbvio para a maioria) deve ser única para cada linha da tabela.
+            <br/>
+            Ela vai definir a ordem dos dados (TODO: entrar mais a fundo aqui).
+            <br/>
+            As clustering columns são armazenadas de forma ordenada e aninhada (nested) uma dentro da outra.
+            <br/>
+            (TODO: colocar exemplos) Não se pode colocar uma cláusula de filtro ("where") usando uma clustering column aninhada, sem referenciar as anteriores. Daí a menção que a ordem das colunas é importante na tabela.
+          </p>
+          <h3>Quais as consultas que temos mais interesse?</h3>
+          <h4>Vamos analisar quais estão conformes de acordo com as sugestões de</h4>
+          <h4>(cada 1 query deve) pegar dados de 1 partição somente</h4>
+          <h4>evitar os "full table scans" (buscas através de TODAS as partições)</h4>
+          <ol>
+            <li>NÃO -- [todos os] Posts em ordem data decrescente</li>
+            <li>NÃO -- Posts [de data X até Y] em ordem data decrescente </li>
+            <li>NÃO -- Posts [de assunto X] em ordem data decrescente </li>
+            <li>NÃO -- Posts [com "isFavorite" true] em ordem data decrescente </li>
+          </ol>
+          <code>
+            (
+            post_creation_date timestamp,
+            post_timestamp timestamp,
+            post_id UUID,
+            post_title text,
+            post_body text,
+            post_image text,
+            post_is_favorite boolean,
+            post_category text,
+            PRIMARY KEY ((post_creation_date), post_timestamp, post_id)
+            )
+          </code>
+        </section>
         <ListaDeArtigos />
       </div>
     </section>
