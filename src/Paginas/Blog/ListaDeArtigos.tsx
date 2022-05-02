375dbe4b107d60bfabfb06cf446116fbaf2a018b
Daniel Santos <iam@w-b.dev>
2022-05-01 23:37:29 -0300

    add filtering

--- src/Paginas/Blog/ListaDeArtigos.tsx
+++ src/Paginas/Blog/ListaDeArtigos.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import "./ListaDeArtigos.css";
 import { ArtigoIndividual } from "./ArtigoIndividual";
 import { useDataApi } from "./UseDataApi";
@@ -8,12 +8,13 @@ export const ListaDeArtigos = () => {
     process.env.REACT_APP_API_URL ?? "https://api.w-b.dev/blog/",
     []
   );
+  const [categorias, setCategorias] = useState<string[]>([]);
+  const [filter, setFilter] = useState<string | null>(null);
   useEffect(() => {
-    setArtigos([...data])
-    buildCategories(data)
-  }, [data])
-  const [artigos, setArtigos] = useState([...data])
-  const [categorias, setCategorias] = useState([])
+    setArtigos([...data]);
+    setCategorias(buildCategories(data));
+  }, [data]);
+  const [artigos, setArtigos] = useState([...data]);
   // const artigos: any[] = [...data];
   const compareDescendingTimestamps = (a: any, b: any) => {
     // Note: `a - b` generates an ASCENDING ordering
@@ -22,17 +23,38 @@ export const ListaDeArtigos = () => {
   return (
     <section className={"ListaDeArtigos"}>
       <div>
-        {
-          categorias.map(el => <button onClick={() => setCategorias(el)}>{el}</button>)
-        }
+        {categorias.map((el) => (
+          <button
+            key={el}
+            className={"botao-filtro"}
+            onClick={() => setFilter(el)}
+          >
+            {el}
+          </button>
+        ))}
       </div>
-      {artigos.sort(compareDescendingTimestamps).map((el: any, i: number) => (
-        <ArtigoIndividual key={el.post_id} artigo={el} numeroAtual={artigos.length - i} />
-      ))}
+      {artigos
+        .filter((el) =>
+          filter ? el.post_category.toLocaleUpperCase().includes(filter) : true
+        )
+        .sort(compareDescendingTimestamps)
+        .map((el: any, i: number) => (
+          <ArtigoIndividual
+            key={el.post_id}
+            artigo={el}
+            numeroAtual={artigos.length - i}
+          />
+        ))}
     </section>
   );
 };
-function buildCategories(data: any[]) {
-    console.log(data)
+function buildCategories(categorias: any[]): string[] {
+  const categoriasSet = new Set<string>();
+  categorias.forEach((categoria) => {
+    const categoryArray = categoria.post_category.split(", "); //First attempt to clear the cat string with space
+    categoryArray.forEach((filteredCat: string) =>
+      categoriasSet.add(filteredCat.toLocaleUpperCase())
+    );
+  });
+  return Array.from(categoriasSet).sort((a, b) => (a > b ? 1 : -1));
 }
-
